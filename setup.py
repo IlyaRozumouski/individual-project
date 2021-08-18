c228606284c2e351aaf0490951f2e1dffd2e7215
IlyaRozumouski <85608022+IlyaRozumouski@users.noreply.github.com>
2021-08-18 17:08:54 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Department-app',
+    version='0.1',
+    description="Application for managing departments and employees",
+    author="IlyaRozumouski",
+    author_email='ilya_razumouski@epam.com',
+    url='https://github.com/IlyaRozumouski/individual-project/tree/irazumouski/department-app',
+    license='BSD-3-Clause',
+)
